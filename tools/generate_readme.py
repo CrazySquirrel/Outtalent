50b639f34cf64363d788c19eaa89f29fe97471a5
Sergei Iastrebov <info@crazysquirrel.ru>
2020-05-17 17:37:59 +0300

    build(maintenance): readme generator for sites

--- tools/generate_readme.py
+++ tools/generate_readme.py
@@ -8,26 +8,37 @@ GIT_ULR_PREFIX = 'https://github.com/CrazySquirrel/Outtalent/tree/master'
 
 SITES = ['Leetcode']
 
-result = [
+global_readme = [
     '# Algorithms, data structures and programming solutions.',
     ''
 ]
 
 for site in SITES:
-    result.append('* [%s](%s/%s)' % (site, GIT_ULR_PREFIX, urllib.parse.quote(site)))
+    site_path = os.path.join(root_path, site)
+
+    global_readme.append('* [%s](%s/%s)' % (site, GIT_ULR_PREFIX, urllib.parse.quote(site)))
+
+    site_readme = [
+        '# %s' % (site),
+        ''
+    ]
 
     tasks = []
 
-    for file in os.listdir(os.path.join(root_path, site)):
-        if os.path.isdir(os.path.join(root_path, site, file)):
+    for file in os.listdir(site_path):
+        if os.path.isdir(os.path.join(site_path, file)):
             tasks.append(file)
 
     for task in sorted(tasks, key=lambda x: int(x.split('.')[0])):
-        result.append('\t* [%s](%s/%s/%s)' % (task, GIT_ULR_PREFIX, site, urllib.parse.quote(task)))
+        global_readme.append('\t* [%s](%s/%s/%s)' % (task, GIT_ULR_PREFIX, site, urllib.parse.quote(task)))
+        site_readme.append('* [%s](%s/%s/%s)' % (task, GIT_ULR_PREFIX, site, urllib.parse.quote(task)))
 
-    result.append('')
+    global_readme.append('')
+
+    with open(os.path.join(site_path, 'README.md'), 'w') as readme:
+        readme.write('\r\n'.join(site_readme))
 
 with open(os.path.join(root_path, 'README.md'), 'w') as readme:
-    readme.write('\r\n'.join(result))
+    readme.write('\r\n'.join(global_readme))
 
 print('README.md UPDATED!!!')
